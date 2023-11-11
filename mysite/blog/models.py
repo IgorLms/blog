9dbb3988ae776c8ef06e9d8dd9f997a022a6ccb1
IgorLms <sigor97@mail.ru>
2023-11-11 23:41:57 +0300

    Создал модель БД - Post

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    """Модель поста блога"""
+
+    class Status(models.TextChoices):
+        DRAFT = 'Черновик'
+        PUBLISHED = 'Опубликована'
+
+    title = models.CharField(max_length=250, verbose_name='Заголовок поста')
+    slug = models.SlugField(max_length=250, verbose_name='Slug поста')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts', verbose_name='Автор поста')
+    body = models.TextField(verbose_name='Пост')
+
+    publish = models.DateTimeField(default=timezone.now, verbose_name='Дата публикации поста')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания поста')
+    update = models.DateTimeField(auto_now=True, verbose_name='Дата изменения поста')
+    status = models.CharField(max_length=12, choices=Status.choices, default=Status.PUBLISHED, verbose_name='Статус поста')
+
+    class Meta:
+        """Упорядочим посты по убыванию даты публикации поста и настроим индексацию по этому полю"""
+        ordering = ['-publish']
+        indexes = [models.Index(fields=['-publish'])]
+
+    def __str__(self):
+        return self.title
+
+
