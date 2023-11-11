fcc657089b5344844b53492111da707dc0650cd2
IgorLms <sigor97@mail.ru>
2023-11-12 00:37:14 +0300

    Модернезировал админ-панель Django, для более удобной работы с таблицей post

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Модернизируем админ-панель для таблицы Post"""
+
+    # Горизонтальное отображение полей поста
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    # Панель фильтрации
+    list_filter = ['status', 'created', 'publish', 'author']
+    # Поле поиска
+    search_fields = ['title', 'body']
+    # Автозаполнение slug
+    prepopulated_fields = {'slug': ('title',)}
+    # Поиск автора
+    raw_id_fields = ['author']
+    # Навигация по дате
+    date_hierarchy = 'publish'
+    # Сортировка
+    ordering = ['status', 'publish']
