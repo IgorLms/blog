c9521c3231be9dc84f38d0e90c08cae8b6c33470
IgorLms <sigor97@mail.ru>
2023-11-12 23:06:00 +0300

    Добавил модель профиля пользователя

--- mysite/accounts/models.py
+++ mysite/accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    """Модель профиля пользователя"""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_images', verbose_name='Аватар')
+    bio = models.TextField(verbose_name='Информация о пользователе')
+
+    class Meta:
+        ordering = ['user']
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
+    def __str__(self):
+        return self.user.username
